d70f268afab11e7a26e7e03e5ad68e57eed9c5ed
daisyisibor <daisyisibor8@gmail.com>
2024-06-16 19:35:10 -0400

    added logged as false

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,15 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+    // For Heroku JawsDB MySQL add-on or similar
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+    // Local MySQL database configuration
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306
+        port: 3306,
+        logging: false
     });
 }
 

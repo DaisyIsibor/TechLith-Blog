e5259bdb21db93db3ecf45aa330df83104299049
daisyisibor <daisyisibor8@gmail.com>
2024-06-17 00:29:52 -0400

    added authentication

--- config/connection.js
+++ config/connection.js
@@ -18,4 +18,14 @@ if (process.env.JAWSDB_URL) {
     });
 }
 
+// Test database connection
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 module.exports = sequelize;

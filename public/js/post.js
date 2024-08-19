3f8a10aac15d480d1c7fde09986ceb2b376db285
daisyisibor <daisyisibor8@gmail.com>
2024-08-19 10:53:51 -0400

    added structringon profile.hbs to handle the rending of liked post on profile and also chnaged singlepost for title to include user name updated to post.user.username

--- public/js/post.js
+++ public/js/post.js
@@ -1,46 +1,5 @@
 // // Function to handle form submission for creating a new post
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     // Function to handle form submission for creating a new post
-//     const newPostFormHandler = async (event) => {
-//         event.preventDefault();
-
-//         const title = document.querySelector('#title').value.trim();
-//         const content = document.querySelector('#content').value.trim();
-
-//         if (title && content) {
-//             try {
-//                 const response = await fetch('/api/posts', {
-//                     method: 'POST',
-//                     headers: {
-//                         'Content-Type': 'application/json'
-//                     },
-//                     body: JSON.stringify({ title, content })
-//                 });
-
-//                 if (response.ok) {
-//                     const responseData = await response.json();
-//                     const postId = responseData.id;
-
-//                     // Redirect to the homepage with the post ID as a URL parameter
-//                     window.location.href = `/?postId=${postId}`;
-//                 } else {
-//                     const responseData = await response.json();
-//                     alert(`Error: ${responseData.error}`);
-//                 }
-//             } catch (error) {
-//                 console.error('Error:', error);
-//                 alert('An error occurred while processing your request.');
-//             }
-//         }
-//     };
-
-//     document.querySelector('#newpost-form').addEventListener('submit', newPostFormHandler);
-
-//     // document.querySelector('#edit-post-form').addEventListener('submit', editPostFormHandler);
-// });
-
-
 document.addEventListener('DOMContentLoaded', () => {
     // Function to handle form submission for creating a new post
     const newPostFormHandler = async (event) => {

5b097b5d2e02c1b1013e2b411058585bfc7985d0
daisyisibor <daisyisibor8@gmail.com>
2024-08-26 21:20:56 -0400

    corrected the nameing of uder_id on the likes seed and ran seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,23 +1,68 @@
-const seedPosts = require('./postSeeds');
+const sequelize = require('../config/connection');
+// const { User, Post, Comment, Like } = require('../models');
 const seedUsers = require('./userSeeds');
+const seedPosts = require('./postSeeds');
 const seedComments = require('./commentSeeds');
 const seedLikes = require('./likeSeeds');
 
-const sequelize = require('../config/connection')
-
 const seedAll = async () => {
-await sequelize.sync({ force: true });
+  try {
+    // Disable foreign key checks to drop tables with constraints
+    await sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
+
+    // Drop all tables
+    await sequelize.dropAllSchemas();
+
+    // Sync the database
+    await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
-await seedUsers();
+
+    // Re-enable foreign key checks
+    await sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
+
+    // Seed the data
+    await seedUsers();
     console.log('\n----- USERS SEEDED -----\n');
-await seedPosts();
+    await seedPosts();
     console.log('\n----- POSTS SEEDED -----\n');
-await seedComments();
+    await seedComments();
     console.log('\n----- COMMENTS SEEDED -----\n');
     await seedLikes();
     console.log('\n----- LIKES SEEDED -----\n');
 
-process.exit(0);
+    process.exit(0);
+  } catch (err) {
+    console.error('Error seeding data:', err);
+    process.exit(1);
+  }
 };
 
 seedAll();
+
+
+
+
+
+// const seedPosts = require('./postSeeds');
+// const seedUsers = require('./userSeeds');
+// const seedComments = require('./commentSeeds');
+// const seedLikes = require('./likeSeeds');
+
+// const sequelize = require('../config/connection')
+
+// const seedAll = async () => {
+// await sequelize.sync({ force: true });
+//     console.log('\n----- DATABASE SYNCED -----\n');
+// await seedUsers();
+//     console.log('\n----- USERS SEEDED -----\n');
+// await seedPosts();
+//     console.log('\n----- POSTS SEEDED -----\n');
+// await seedComments();
+//     console.log('\n----- COMMENTS SEEDED -----\n');
+//     await seedLikes();
+//     console.log('\n----- LIKES SEEDED -----\n');
+
+// process.exit(0);
+// };
+
+// seedAll();
--- seeds/likeSeeds.js
+++ seeds/likeSeeds.js
@@ -2,11 +2,11 @@ const { Like } = require('../models');
 
 const likeData = [
 {
-    userId: 1,
+    user_id: 1,
     postId: 1,
 },
 {
-    userId: 2,
+    user_id: 2,
     postId: 1,
 },
 
